2af429b4520ae69e2044bc3379bd3327dda55f93
MaxRussell42 <maxarussell@gmail.com>
2017-03-19 20:22:19 -0500

    Fixed TextExcel

--- TextExcel/src/textExcel/TextExcel.java
+++ TextExcel/src/textExcel/TextExcel.java
@@ -7,10 +7,13 @@ public class TextExcel
 
 	public static void main(String[] args)
 	{
-		 Scanner userInput = new Scanner(System.in);
-		    String input = userInput.nextLine();
-		    while (input != "quit"){
-		    	
+		Spreadsheet spreadsheet = new Spreadsheet();
+		Scanner userInput = new Scanner(System.in);		//create scanner
+		String input = userInput.nextLine();			
+		String command = "";					//create string for imput commands
+		while (input != "quit"){				//loop for command processing
+		    	command = input;
+			System.out.println(spreadsheet.processCommand(command));
 		    }
 	}
 }
