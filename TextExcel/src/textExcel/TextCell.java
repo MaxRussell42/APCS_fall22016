f3aa34e41725ad165300ee26048c0e968a2c9ede
MaxRussell42 <maxarussell@gmail.com>
2017-03-19 20:13:18 -0500

    Updated TextCell

--- TextExcel/src/textExcel/TextCell.java
+++ TextExcel/src/textExcel/TextCell.java
@@ -3,15 +3,21 @@ package textExcel;
 public class TextCell {
 	private String textCell;
 	public TextCell(String inputText){
-		textCell = inputText;
+		textCell = inputText;					//base string for manipulation
 	}
 	
 	public String abbreviatedCellText(){
-		String abbreviatedCell = ((textCell+"          ".substring(0,10)));
-		return abbreviatedCell;
+		String abbreviatedCell = textCell;
+		if(abbreviatedCell.length()<10){			//makes cell equal 10
+			while(abbreviatedCell.length()<10){
+				abbreviatedCell += " "; 
+			}
+			return abbreviatedCell;
+		} else {
+		return abbreviatedCell;					//returns if already true
 	}
-	public String fullCellText(){
-		return textCell;
+	public String fullCellText(){		
+		return textCell;					//no change needed 
 	}
 	
 }
